48f669415e0c446f23e0e5bafb7a52b108c8505a
Shashi <shashi717671@gmail.com>
2026-05-07 15:02:40 +0530

    remove dist, add to gitignore

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/VoltMap_India_frontend/',
   plugins: [react()],
-  base: "/VoltMapIndia_frontend/",
 })
